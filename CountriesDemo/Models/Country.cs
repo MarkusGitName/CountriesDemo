4773aaebca9fcd53a4d4c02dc8dd13dd7b3a8971
Markus Madeleyn <Markus.Madeleyn@xpertek.co.za>
2022-08-18 21:16:51 +0200

    Add project files.

---
+++ CountriesDemo/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CountriesDemo.Models
+{
+    public class Country
+    {
+        
+        public Guid Id { get; set; }
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+        [Display(Name = "Alpha2")]
+        public string? Alpha2 { get; set; }
+        [Display(Name = "Alpha3")]
+        public string? Alpha3 { get; set; }
+        [Display(Name = "Numeric")]
+        public string? Numeric { get; set; }
+        [Display(Name = "Active")]
+        public bool Active { get; set; }
+        public ICollection<Currency>? Currencies { get; set; }
+    }
+}
---
+++ CountriesDemo/Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CountriesDemo.Models
+{
+    public class Currency
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Active")]
+        public bool Active { get; set; }
+        public ICollection<Country>? Countries { get; set; }
+
+    }
+}
